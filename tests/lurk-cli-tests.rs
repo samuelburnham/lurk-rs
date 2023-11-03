1aa030393d8713516e99a0efdf276de193b8190d
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-11-03 08:55:54 -0400

    [WIP] Add CLI integration tests

--- tests/lurk-cli-tests.rs
+++ tests/lurk-cli-tests.rs
@@ -43,6 +43,44 @@ fn test_bad_command() {
     cmd.assert().failure();
 }
 
+// Tests that commitments correctly persist on the file system between `lurk` invocations
+#[test]
+fn test_commit_and_open() {
+    let tmp_dir = Builder::new().prefix("tmp").tempdir().unwrap();
+    let tmp_dir = Utf8Path::from_path(tmp_dir.path()).unwrap();
+    let commit_dir = tmp_dir.join("commits");
+    let commit_file = tmp_dir.join("commit.lurk");
+    let open_file = tmp_dir.join("open.lurk");
+
+    let mut file = File::create(commit_file.clone()).unwrap();
+    file.write_all(b"!(commit (+ 1 1))\n").unwrap();
+
+    let mut file = File::create(open_file.clone()).unwrap();
+    file.write_all(b"!(open 0x0973b895ba6991037628d9d3a5661152e034abcd459b61be9faa4b67304774c6)\n")
+        .unwrap();
+
+    let mut cmd = lurk_cmd();
+    cmd.arg("load");
+    cmd.arg(commit_file.into_string());
+    cmd.arg("--commits-dir");
+    cmd.arg(&commit_dir);
+    assert_eq!(
+        &String::from_utf8(cmd.output().unwrap().stdout)
+            .unwrap()
+            .lines()
+            .collect::<Vec<String>>(),
+        "Data: 2\nHash: 0x0973b895ba6991037628d9d3a5661152e034abcd459b61be9faa4b67304774c6"
+    );
+    //assert().success();
+
+    let mut cmd = lurk_cmd();
+    cmd.arg("load");
+    cmd.arg(open_file.into_string());
+    cmd.arg("--commits-dir");
+    cmd.arg(commit_dir);
+    cmd.assert().success();
+}
+
 // TODO: Use a snapshot test for the proof ID and/or test the REPL process
 #[test]
 fn test_prove_and_verify() {
@@ -69,3 +107,13 @@ fn test_prove_and_verify() {
 
     cmd.assert().success();
 }
+
+//#[test]
+//fn test_chained_fcomm() {
+//    let tmp_dir = Builder::new().prefix("tmp").tempdir().unwrap();
+//    let commit_dir = tmp_dir.join("commits");
+//
+//    let mut cmd = lurk_cmd();
+//    cmd.arg("commit");
+//    cmd.assert().failure();
+//}
